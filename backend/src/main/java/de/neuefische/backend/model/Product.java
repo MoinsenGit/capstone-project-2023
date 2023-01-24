30702f8080695891ba6c3f0d75f0c13c591a1bb6
MoinsenGit <nachts@freenet.de>
2023-01-24 16:16:08 +0100

    feature: sonar functionality

---
+++ backend/src/main/java/de/neuefische/backend/model/Product.java
@@ -0,0 +1,12 @@
+package de.neuefische.backend.model;
+
+import java.awt.*;
+import java.math.BigDecimal;
+
+public class Product {
+    private String id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private Image image; // ToDo Image is a class from the java.awt package - correct?
+}
